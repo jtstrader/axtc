b801d81f82cc8ec7922700d5e8385a62f06ca116
jtstrader <jacob.thomas.strader@gmail.com>
2025-05-15 15:43:29 -0400

    feat: Scaffold command runner

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+/// Save the current theme with the provided name.
+pub fn save(new_theme_name: String) {}
+
+/// Load the provided theme. If `unsafe_load` is `false`, save the current theme in the recovery
+/// section.
+pub fn load(theme_name: String, unsafe_load: bool, recovery: bool) {}
+
+/// List the available themes.
+pub fn list(recovery: bool) {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use clap::{Parser, Subcommand};
+
+/// All available options for AXTC.
+#[derive(Subcommand, Debug, Clone)]
+enum Options {
+    /// Save the current theme.
+    Save {
+        /// The name of the new theme. Must not already exist in the theme list.
+        new_name: String,
+    },
+
+    /// Load the given theme.
+    Load {
+        /// The name of the theme to load. It must exist in the theme list.
+        theme: String,
+
+        /// Load the theme without saving a copy of the current theme.
+        #[clap(name = "unsafe", long, default_value_t = false)]
+        unsafe_load: bool,
+
+        /// Include searches in the recovery section.
+        #[clap(long, default_value_t = false)]
+        recovery: bool,
+    },
+
+    /// List all supported themes.
+    List {
+        /// Include recovered themes in the listing.
+        #[clap(long, default_value_t = false)]
+        recovery: bool,
+    },
+}
+
+/// An Arch/X theme changer.
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(subcommand)]
+    opt: Options,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    match args.opt {
+        Options::Save { new_name } => axtc_lib::save(new_name),
+        Options::Load {
+            theme,
+            unsafe_load,
+            recovery,
+        } => axtc_lib::load(theme, unsafe_load, recovery),
+        Options::List { recovery } => axtc_lib::list(recovery),
+    }
 }
